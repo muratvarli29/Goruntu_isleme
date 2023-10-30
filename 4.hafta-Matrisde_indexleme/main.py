a340edef6e4b4218b403c302559f54d686dbccb6
muratvarli29 <varlii29@hotmail.com>
2023-10-30 16:11:46 +0300

    matrisde indexleme işlemi

---
+++ 4.hafta-Matrisde_indexleme/main.py
@@ -0,0 +1,10 @@
+import numpy as np
+a=np.array([[1,4,5]
+            ,[-5,8,9]
+            ,[6,8,10]
+            ,[0,2,38]])
+print("type :",type(a))
+print("a =","\n",a,"\n")
+print("a[1]",a[1])
+print("a[1][2] =",a[1][2])
+print("a[0][-1] =",a[0][-1])
