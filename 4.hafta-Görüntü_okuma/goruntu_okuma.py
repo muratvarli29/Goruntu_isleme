7b963bc153b878854daba6426c4b23a292be73c2
muratvarli29 <varlii29@hotmail.com>
2023-10-30 16:10:02 +0300

    Goruntu okuma

---
+++ 4.hafta-Görüntü_okuma/goruntu_okuma.py
@@ -0,0 +1,9 @@
+import cv2
+import numpy as np
+resim1=cv2.imread("4.jpg")
+cv2.imshow("Brain",resim1)
+print(resim1.size)
+print(resim1.dtype)
+print(resim1.shape)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
